a3d7c61c37244863d918217ecb50a10cb1a0a70a
dairlair <51401608+dairlair@users.noreply.github.com>
2020-07-20 12:10:32 +0300

    Test coverage for memorize() function

--- facekeeper/facekeeper.py
+++ facekeeper/facekeeper.py
@@ -6,14 +6,25 @@ from typing import List, Optional
 
 
 class FaceKeeperError(Exception):
+    """
+        Just a general error for FaceKeeper
+    """
     pass
 
 
 class FaceNotFoundError(FaceKeeperError):
+    """
+        This error must be raised when the recognizer can not recognize face in the given image
+    """
     pass
 
 
 class PersonEmbedding:
+    """
+        The person embedding is structure which contains information about person
+        and the vector representation of his face features
+    """
+
     def __init__(self, person: str, embedding: np.array) -> None:
         self.person = person
         self.embedding = embedding
@@ -29,41 +40,59 @@ class StorageInterface(ABC):
     """
 
     @abstractmethod
-    def add_embedding(self, person: str, image_digest: str, recognizer: str, embedding: np.array) -> None:
+    def save_embedding(self, person: str, image_digest: str, recognizer: str, embedding: np.array) -> None:
         """
         :param person: The unique person identifier.
         :param image_digest: Image digest calculated with some hash function. Used to avoid duplicates
-        :param recognizer: Various embedder (algorithms or trained model) produces various embeddings for
-                            the same face. We need to take it into account.
+        :param recognizer: The unique identifier of neural network and trained model used to embedding calculation
         :param embedding: Face embedding (e.g.: 128-dimensional vector)
-        :return:
         """
         pass
 
     @abstractmethod
-    def get_embeddings(self, recognizer) -> List[PersonEmbedding]:
+    def get_embeddings(self, recognizer: str) -> List[PersonEmbedding]:
+        """
+        This method must retrieves from the storage all embeddings made with specified recognizer
+        :param recognizer: The unique identifier of neural network and trained model used to embedding calculation
+        """
         pass
 
 
 class RecognizerInterface(ABC):
     """
-        EmbedderInterface declares dependency from the library which is used to calculate face embeddings (encodings)
+        RecognizerInterface declares dependency from the library which is used to calculate face embeddings (encodings)
     """
 
     @abstractmethod
     def get_id(self) -> str:
+        """
+        Every new implementation of RecognizerInterface must returns unique ID. Every new trained model
+        used inside must returns the new ID as well, cause we can not compare face embeddings received calculated
+        with parameters.
+        """
         pass
 
     @abstractmethod
     def calc_embedding(self, image: bytes) -> np.array:
+        """
+        Must returns face embedding calculated with certain trained model.
+        """
         pass
 
     @abstractmethod
     def add_embeddings(self, embeddings: List[PersonEmbedding]) -> None:
+        """
+        Must adds embeddings to the internal storage of recognizer.
+        They will be used for face recognition.
+        """
         pass
 
     @abstractmethod
-    def recognise(self, image: bytes) -> str:
+    def recognize(self, image: bytes) -> str:
+        """
+        Must calculate face embeddings and try to find similar embedding among the known person embeddings
+        to identify the person from the image.
+        """
         pass
 
 
@@ -82,13 +111,18 @@ class RecognizeResponse:
 
 
 class FaceKeeper:
+    initialized: bool = False
+
     @inject
     def __init__(self, recognizer: RecognizerInterface, storage: StorageInterface):
-        self.initialized = False
         self.recognizer = recognizer
         self.storage = storage
 
     def initialize(self):
+        """
+        Retrieves all known embeddings which suits current recognizer from the storage and load them
+        into the recognizer.
+        """
         embeddings = self.storage.get_embeddings(self.recognizer.get_id())
         self.recognizer.add_embeddings(embeddings)
         self.initialized = True
@@ -97,12 +131,18 @@ class FaceKeeper:
         return self.initialized
 
     def memorize(self, person: str, image: bytes) -> MemorizeResponse:
+        """
+        Takes the person identifier and the picture.
+        Calculates the face embeddings for face on the photo and remember this embeddings as related with person.
+
+        Note: if the image contains zero or more than one faces this method will raise error: FaceNotFoundError.
+        """
         digest = get_digest(image)
         recognizer = self.recognizer.get_id()
         embedding = self.recognizer.calc_embedding(image)
 
         # Save calculated embedding in the storage
-        self.storage.add_embedding(person, digest, recognizer, embedding)
+        self.storage.save_embedding(person, digest, recognizer, embedding)
 
         # Load calculated embedding into the recognizer embeddings
         self.recognizer.add_embeddings([PersonEmbedding(person, embedding)])
@@ -110,6 +150,11 @@ class FaceKeeper:
         return MemorizeResponse(digest)
 
     def recognize(self, image: bytes) -> Optional[RecognizeResponse]:
-        person = self.recognizer.recognise(image)
+        """
+        Tries to find the similar embeddings and returns the person identifier if it is found, None otherwise.
+
+        Note: if the image contains zero or more than one faces this method will raise error: FaceNotFoundError.
+        """
+        person = self.recognizer.recognize(image)
 
         return RecognizeResponse(person=person) if person else None
--- tests/test_facekeeper.py
+++ tests/test_facekeeper.py
@@ -2,13 +2,13 @@ from mock import Mock, create_autospec
 from typing import List
 import numpy as np
 
-from facekeeper.facekeeper import FaceKeeper, RecognizerInterface, StorageInterface, PersonEmbedding
+from facekeeper.facekeeper import FaceKeeper, RecognizerInterface, StorageInterface, PersonEmbedding, get_digest
 
 
 def test_initialization():
     # Given
     recognizer_id = 'Any Recognizer ID'
-    embeddings: List[PersonEmbedding] = [PersonEmbedding('Morning Star', np.array([1, 2, 3]))]
+    embeddings: List[PersonEmbedding] = [PersonEmbedding('Agent Smith', np.array([1, 2, 3]))]
 
     recognizer = create_autospec(RecognizerInterface)
     recognizer.get_id = Mock(return_value=recognizer_id)
@@ -25,3 +25,33 @@ def test_initialization():
     storage.get_embeddings.assert_called_once_with(recognizer_id)
     recognizer.add_embeddings.assert_called_once_with(embeddings)
     assert facekeeper.is_initialized()
+
+
+# Ensure that when we call memorize method the FaceKeeper will:
+#   1. calculate embeddings for given image
+#   2. store the embeddings in the Storage and
+#   3. add them into to the currently running Recognizer
+def test_memorize():
+    # Given
+    recognizer_id: str = 'Any Recognizer ID'
+    person: str = 'John Smith'
+    image: bytes = bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])
+    digest = get_digest(image)
+    embedding: np.array = np.array([1, 2, 3])
+
+    recognizer = create_autospec(RecognizerInterface)
+    recognizer.get_id = Mock(return_value=recognizer_id)
+    recognizer.calc_embedding = Mock(return_value=embedding)
+
+    storage = create_autospec(StorageInterface)
+
+    facekeeper = FaceKeeper(recognizer, storage)
+
+    # When
+    facekeeper.memorize(person, image)
+
+    # Then
+    recognizer.get_id.assert_called_once()
+    recognizer.calc_embedding.assert_called_once_with(image)
+    recognizer.add_embeddings.assert_called_once()
+    storage.save_embedding.assert_called_once_with(person, digest, recognizer_id, embedding)
