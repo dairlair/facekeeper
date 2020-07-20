ab0193333db1918187e79220b65041f8ee229a21
dairlair <51401608+dairlair@users.noreply.github.com>
2020-07-20 14:52:32 +0300

    Initial app draft (#1)
    The application draft is ready.

---
+++ facekeeper/facekeeper.py
@@ -0,0 +1,160 @@
+from abc import ABC, abstractmethod
+import numpy as np
+import hashlib
+from injector import inject
+from typing import List, Optional
+
+
+class FaceKeeperError(Exception):
+    """
+        Just a general error for FaceKeeper
+    """
+    pass
+
+
+class FaceNotFoundError(FaceKeeperError):
+    """
+        This error must be raised when the recognizer can not recognize face in the given image
+    """
+    pass
+
+
+class PersonEmbedding:
+    """
+        The person embedding is structure which contains information about person
+        and the vector representation of his face features
+    """
+
+    def __init__(self, person: str, embedding: np.array) -> None:
+        self.person = person
+        self.embedding = embedding
+
+
+class StorageInterface(ABC):
+    """
+        StorageInterface is used to declare dependency from the storage.
+
+        The storage must provide functionality to:
+            * add face embedding for specified persons
+            * retrieve all embeddings
+    """
+
+    @abstractmethod
+    def save_embedding(self, person: str, image_digest: str, recognizer: str, embedding: np.array) -> None:
+        """
+        :param person: The unique person identifier.
+        :param image_digest: Image digest calculated with some hash function. Used to avoid duplicates
+        :param recognizer: The unique identifier of neural network and trained model used to embedding calculation
+        :param embedding: Face embedding (e.g.: 128-dimensional vector)
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_embeddings(self, recognizer: str) -> List[PersonEmbedding]:
+        """
+        This method must retrieves from the storage all embeddings made with specified recognizer
+        :param recognizer: The unique identifier of neural network and trained model used to embedding calculation
+        """
+        raise NotImplementedError
+
+
+class RecognizerInterface(ABC):
+    """
+        RecognizerInterface declares dependency from the library which is used to calculate face embeddings (encodings)
+    """
+
+    @abstractmethod
+    def get_id(self) -> str:
+        """
+        Every new implementation of RecognizerInterface must returns unique ID. Every new trained model
+        used inside must returns the new ID as well, cause we can not compare face embeddings received calculated
+        with parameters.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def calc_embedding(self, image: bytes) -> np.array:
+        """
+        Must returns face embedding calculated with certain trained model.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_embeddings(self, embeddings: List[PersonEmbedding]) -> None:
+        """
+        Must adds embeddings to the internal storage of recognizer.
+        They will be used for face recognition.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def recognize(self, image: bytes) -> str:
+        """
+        Must calculate face embeddings and try to find similar embedding among the known person embeddings
+        to identify the person from the image.
+        """
+        raise NotImplementedError
+
+
+def get_digest(data: bytes) -> str:
+    return hashlib.sha256(data).hexdigest()
+
+
+class MemorizeResponse:
+    def __init__(self, digest: str):
+        self.digest = digest
+
+
+class RecognizeResponse:
+    def __init__(self, person: str):
+        self.person = person
+
+
+class FaceKeeper:
+    initialized: bool = False
+
+    @inject
+    def __init__(self, recognizer: RecognizerInterface, storage: StorageInterface):
+        self.recognizer = recognizer
+        self.storage = storage
+
+    def initialize(self):
+        """
+        Retrieves all known embeddings which suits current recognizer from the storage and load them
+        into the recognizer.
+        """
+        embeddings = self.storage.get_embeddings(self.recognizer.get_id())
+        self.recognizer.add_embeddings(embeddings)
+        self.initialized = True
+
+    def is_initialized(self):
+        return self.initialized
+
+    def memorize(self, person: str, image: bytes) -> MemorizeResponse:
+        """
+        Takes the person identifier and the picture.
+        Calculates the face embeddings for face on the photo and remember this embeddings as related with person.
+
+        Note: if the image contains zero or more than one faces this method will raise error: FaceNotFoundError.
+        """
+        digest = get_digest(image)
+        recognizer = self.recognizer.get_id()
+        embedding = self.recognizer.calc_embedding(image)
+
+        # Save calculated embedding in the storage
+        self.storage.save_embedding(person, digest, recognizer, embedding)
+
+        # Load calculated embedding into the recognizer embeddings
+        self.recognizer.add_embeddings([PersonEmbedding(person, embedding)])
+
+        return MemorizeResponse(digest)
+
+    def recognize(self, image: bytes) -> Optional[RecognizeResponse]:
+        """
+        Tries to find the similar embeddings and returns the person identifier if it is found, None otherwise.
+
+        Note: if the image contains zero or more than one faces this method will raise error: FaceNotFoundError.
+        """
+        person = self.recognizer.recognize(image)
+
+        return RecognizeResponse(person=person) if person else None
---
+++ tests/test_facekeeper.py
@@ -0,0 +1,77 @@
+from mock import Mock, create_autospec
+from typing import List
+import numpy as np
+
+from facekeeper.facekeeper import FaceKeeper, RecognizerInterface, StorageInterface, PersonEmbedding, get_digest, \
+    RecognizeResponse
+
+
+def test_initialization():
+    # Given
+    recognizer_id = 'Any Recognizer ID'
+    embeddings: List[PersonEmbedding] = [PersonEmbedding('Agent Smith', np.array([1, 2, 3]))]
+
+    recognizer = create_autospec(RecognizerInterface)
+    recognizer.get_id = Mock(return_value=recognizer_id)
+
+    storage = create_autospec(StorageInterface)
+    storage.get_embeddings = Mock(return_value=embeddings)
+    facekeeper = FaceKeeper(recognizer, storage)
+
+    # When
+    facekeeper.initialize()
+
+    # Then
+    recognizer.get_id.assert_called_once()
+    storage.get_embeddings.assert_called_once_with(recognizer_id)
+    recognizer.add_embeddings.assert_called_once_with(embeddings)
+    assert facekeeper.is_initialized()
+
+
+def test_memorize():
+    """
+    Ensure that when we call memorize method the FaceKeeper will:
+      1. calculate embeddings for given image
+      2. store the embeddings in the Storage and
+      3. add them into to the currently running Recognizer
+    """
+    # Given
+    recognizer_id: str = 'Any Recognizer ID'
+    person: str = 'John Smith'
+    image: bytes = bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])
+    digest = get_digest(image)
+    embedding: np.array = np.array([1, 2, 3])
+
+    recognizer = create_autospec(RecognizerInterface)
+    recognizer.get_id = Mock(return_value=recognizer_id)
+    recognizer.calc_embedding = Mock(return_value=embedding)
+
+    storage = create_autospec(StorageInterface)
+
+    facekeeper = FaceKeeper(recognizer, storage)
+
+    # When
+    facekeeper.memorize(person, image)
+
+    # Then
+    recognizer.get_id.assert_called_once()
+    recognizer.calc_embedding.assert_called_once_with(image)
+    recognizer.add_embeddings.assert_called_once()
+    storage.save_embedding.assert_called_once_with(person, digest, recognizer_id, embedding)
+
+
+def test_recognize():
+    # Given
+    person: str = 'John Smith'
+    image: bytes = bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])
+    recognizer = create_autospec(RecognizerInterface)
+    recognizer.recognize = Mock(return_value=person)
+    storage = create_autospec(StorageInterface)
+    facekeeper = FaceKeeper(recognizer, storage)
+
+    # When
+    response = facekeeper.recognize(image)
+
+    # Then
+    assert type(response) is RecognizeResponse
+    assert response.person is person
